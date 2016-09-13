ada5e97aef364802b3b3c62f156502b8f27f169f
Gustavo A. Fernandes <gustavofernandes@users.noreply.github.com>
2016-09-13 17:02:54 -0400

    Add initial aliases

---
+++ aliases.zsh
@@ -0,0 +1,10 @@
+alias ls='ls -AG'
+alias f='open .'
+
+# Buildr
+alias bcp='buildr clean package'
+alias bcpe='buildr clean package eclipse'
+alias bcaspe='buildr clean artifacts:sources package eclipse'
+
+# Servers
+alias ws='python -m SimpleHTTPServer'
