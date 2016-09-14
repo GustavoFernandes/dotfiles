64cce08445b400dd5ce998f31e9bc1ef6a0db2b8
Gustavo A. Fernandes <gustavofernandes@users.noreply.github.com>
2016-09-14 11:07:32 -0400

    Fix string sequences

--- aliases.zsh
+++ aliases.zsh
@@ -6,8 +6,8 @@ alias g='git number'
 alias ga='git number add'
 alias gcm='git commit -m'
 alias gco='git number checkout'
-alias gd='git diff --color-words='\\w+|.'
-alias gdc='git diff --color-words='\\w+|.' --cached
+alias gd='git diff --color-words="\\w+|."'
+alias gdc='git diff --color-words="\\w+|." --cached'
 alias gf='git fetch --all -p'
 
 # Buildr
