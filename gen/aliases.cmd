9c9e80769df8da7f974787023015779c5195b655
Gustavo A. Fernandes <GustavoFernandes@users.noreply.github.com>
2019-05-29 15:22:10 -0700

    Add more aliases

--- gen/aliases.cmd
+++ gen/aliases.cmd
@@ -2,6 +2,9 @@
 
 DOSKEY ga=git add $*
 DOSKEY gb=git branch $*
+DOSKEY gd=git diff
+DOSKEY gdc=git diff --cached
 DOSKEY glog=git log --oneline --graph
 DOSKEY gloga=git log --oneline --graph --all
 DOSKEY gs=git status -sb
+DOSKEY np="C:\Program Files\Notepad++\notepad++.exe" $*
