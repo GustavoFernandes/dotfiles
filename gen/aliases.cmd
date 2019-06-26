1c6d22fd3633f3a8e5fe97e01423baffc9fe170a
Gustavo A. Fernandes <GustavoFernandes@users.noreply.github.com>
2019-06-26 12:51:45 -0700

    More alias work

--- gen/aliases.cmd
+++ gen/aliases.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-DOSKEY ga=git add $*
+DOSKEY ga=git add -v $*
 DOSKEY gb=git branch $*
 DOSKEY gd=git diff $*
 DOSKEY gdc=git diff --cached $*
+DOSKEY gdw=git diff --color-words="\w+|." $*
 DOSKEY glog=git log --oneline --graph $*
 DOSKEY gloga=git log --oneline --graph --all $*
 DOSKEY gs=git status -sb $*
